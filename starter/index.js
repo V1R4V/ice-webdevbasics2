24fe6541b89f02ce55370559928bfce1b6e29c87
Vibhrav Jha <133233332+V1R4V@users.noreply.github.com>
2026-02-01 17:05:05 -0600

    JS functions

--- starter/index.js
+++ starter/index.js
@@ -5,18 +5,27 @@
 //  https://cs571api.cs.wisc.edu/rest/s26/ice/pasta
 //  https://cs571api.cs.wisc.edu/rest/s26/ice/pizza
 
-let reviewNum = 0;
-const REVIEWS = [
-    "A burst of warmth and flavor in every spoonful; simple yet irresistible!",
-    "The perfect blend of spice and comfort, an easy go-to chili recipe.",
-    "Loved the hearty texture and rich taste - a new family favorite!",
-    "Quick, flavorful, and satisfying - this chili hits all the right notes!"
-];
+// This is where your JS goes!
 
-function displayReview() {
-    alert(REVIEWS[reviewNum]);
-    reviewNum = (reviewNum + 1) % REVIEWS.length;
-}
+// const REVIEWS = [
+//     "A burst of warmth and flavor in every spoonful; simple yet irresistible!",
+//     "The perfect blend of spice and comfort, an easy go-to chili recipe.",
+//     "Loved the hearty texture and rich taste - a new family favorite!",
+//     "Quick, flavorful, and satisfying - this chili hits all the right notes!"
+// ]
 
-// TODO Using JavaScript, update the "N" in "Easy N-Ingredient Chili"
-//      to be the number of ingredients in the chili.
+// let reviewNum = 0;
+// function displayReview(){
+// document.getElementById("review-btn").addEventListener("click", (e) => { //adding functions to events
+//     alert(REVIEWS[reviewNum]);
+//     reviewNum = (reviewNum + 1) % REVIEWS.length; //cycles through the 4 reviews and resets to 0 at 4
+// })
+// }
+// //changes texts
+// let title = document.getElementById("recipe-title")
+// //title.innerText = "EASY 7 CHILI recipe"
+
+// //calculates the number of ingredients dynamically 
+// numOfIngrs = document.getElementsByTagName("tbody")[0].getElementsByTagName("tr").length
+// title.innerText = "Easy " + numOfIngrs + "-Ingredient Chili";
+
